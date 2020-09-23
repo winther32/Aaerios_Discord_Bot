274533250ab89d42de14908482b30f8b68a89ffd
maclyle <maclyle32@gmail.com>
2020-09-23 12:51:40 -0700

    help update

--- commands/help.js
+++ commands/help.js
@@ -8,7 +8,7 @@ module.exports = {
     execute(message, args) {
         message.channel.send("**Clippy Bot Command List!**\n\n" + 
                             "**Clips:\n**" + 
-                            "mustard, tarkov, milk\n" + 
+                            "mustard, tarkov, milk, angry\n" + 
                             "**Library:**\n" + 
                             "library, songs, overwrite, add, lookup\n" + 
                             "**Questions:**\n" +
