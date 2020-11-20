449ec3db7c14c1289391c890a6094a01d6a0f28a
maclyle <maclyle32@gmail.com>
2020-11-19 16:48:57 -0800

    lookup now has find feature. Help updated

--- commands/help.js
+++ commands/help.js
@@ -14,7 +14,7 @@ module.exports = {
         const helpEmbed = new Discord.MessageEmbed()
             .setColor('187bcd')
             .setTitle("Clippy Bot Command List")
-            .setDescription("Learn how to use the community managed clip library with \"$help library\"")
+            .setDescription("Learn how to use the community managed clip library with `$help library`")
             .addFields(
                 // {name: '\u200B', value: '\u200B' },
                 {name: 'Clips:', value: "`mustard`, `tarkov`, `milk`, `salsa`"},
@@ -29,10 +29,12 @@ module.exports = {
                 .setDescription("A quick guide to adding to and searching the community managed clip library!")
                 .addFields(
                     {name: 'Why Does This Exist?', value: "`Twitch only organizes clips by all time views. This means that your favorite niche clip can get buried in the plethera of amazing clips! This library while not a perfect solution provides a way to add keywords to a clip and save them in one place so you can find them later!`"},
-                    {name: "Finding A Clip:", value: "`To find your favorite clip first you have to open the GoogleSheet where everything is saved. You can get the link with the $library command. Next use the ctrl+F function to search for the keyword you are looking for.`"},
+                    {name: "The Library Command:", value: "`This command provides a link to the full GoogleSheet where everything is saved. Here you can browse all the links and keywords. I hope to add clip thumbnails to this in the future.`"},
+                    {name: "Finding A Clip With GoogleSheets:", value: "`To find your favorite clip first you have to open the GoogleSheet where everything is saved. You can get the link with the $library command. Next use the ctrl+F function to search for the keyword you are looking for.`"},
+                    {name: "Finding A Clip With The Lookup Command:", value: "`You can also find a clip with the $lookup command followed by comma seperated keywords. Clippy Bot will then list all the clip links that have the given keyword(s). Discord will also show previews for the first 5 links listed.`"},
                     {name: 'Adding A Clip:', value: "`To add a clip simply use the $add command followed by comma seperated keywords and the link to the clip!`"},
                     {name: "Overwriting A Clip:", value: "`If you believe that the keywords paired with a certain clip are wrong or you want to add new keywords you can use the $overwrite command followed by the replacement keywords and the corresponding clip link.`"},
-                    {name: "The Lookup Command:", value: "`This command used with a clip link will check if the clip is already in the library or not. If it is, Clippy Bot will provide the current keywords for that clip.`"},
+                    {name: "The Lookup Command:", value: "`This command searches the database. If you use $lookup followed by a link, Clippy Bot will find the keywords associated with that link. If you use $lookup followed by keywords Clippy Bot will find all the clips with those keywords.`"},
                     {name: "The Songs Command:", value: "`Our Lord Aaerios has blessed us many a time with his beautiful songs. These have been saved all in one place for your enjoyment at the link provided.`"}
                 )
                 .setFooter("If you still have questions feel free to DM Winther");
--- commands/lookup.js
+++ commands/lookup.js
@@ -13,6 +13,7 @@ const doc = new GoogleSpreadsheet('13NWMHvTFKaaeKlu2u7HOkPT84PT-5ARKpsnHAihU26E'
 
 // Function to access sheet via google api
 async function lookup(message, link) {
+    console.log("Begin lookup call");
     // Auth
     await doc.useServiceAccountAuth(creds);
     await doc.loadInfo();
@@ -36,8 +37,36 @@ async function lookup(message, link) {
             return;
         }
     }
+    console.log("no clip found in lookup");
+    message.channel.send("This clip is not yet in the database! Feel free to add it with the $add command.");
 }
 
+// Function to lookup and return a list of clips with given keywords
+async function find(message, args) {
+    await doc.useServiceAccountAuth(creds);
+    await doc.loadInfo();
+
+    // Get the sheet in spreadsheet
+    const sheet = await doc.sheetsByIndex[0];
+
+    // For loop to check for dupicate links
+    var rows = await sheet.getRows();
+    var results = "";
+    // Linear loop to find match if exists
+    for (var i = 0; i < rows.length; i++) {
+        for (var j = 0; j < args.length; j++) {
+            if (rows[i].Keywords.includes(args[j])) {
+                results = results + rows[i].Clip + "\n";
+                break;
+            }
+        }
+    }
+    if (results == "") {
+        message.channel.send("No clips found with the keyword(s): " + args);
+    } else {
+        message.channel.send(results);
+    }
+}
 
 module.exports = {
     name: 'lookup',
@@ -45,7 +74,7 @@ module.exports = {
     execute(message, args) {
         // Verify got args
         if (args.length < 1) {
-            message.channel.send('Invalid format\n**Usage:** $lookup <link>');
+            message.channel.send('Invalid format\n**Usage:** $lookup <link> **OR** $lookup <comma seperated keywords>');
             return;
         }
         var link = args[0]; // Get full link from args
@@ -59,7 +88,11 @@ module.exports = {
                 await lookup(message, link);
             })();   
         } else {
-            message.channel.send('Invalid format\n**Usage:** $lookup <link>');
+            message.channel.send("Looking for clips with keyword(s): " + args);
+            // Start the find function
+            (async() => {
+                await find(message, args);
+            })();
         }
     }
 }
